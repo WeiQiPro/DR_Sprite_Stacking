231d5a937e02f8ab98bffe4c7e37c2b8b03364a0
Jeremiah Donley <108106416+WeiQiPro@users.noreply.github.com>
2024-05-05 02:04:07 -0400

    Update main.rb

--- main.rb
+++ main.rb
@@ -1,12 +1,12 @@
 def tick args
   if args.tick_count == 0
-    args.state.dr_logo ||= {
+    args.state.sprite_stack ||= {
       x: 640 - (100),
       y: 360 - (100),
       stack: 50,
       w: 50,
       h: 50,
-      path: "sprites/KingsRoomByMaxParata.png"
+      path: "sprites/stack/KingsRoomByMaxParata.png"
     }
     args.state.camera_angle ||= 90
 
@@ -14,24 +14,25 @@ def tick args
 
  args.state.camera_angle += args.inputs.up_down_with_wasd * 5
 
- dr_logo = args.state.dr_logo
+ sprite_stack = args.state.sprite_stack
  camera_angle = args.state.camera_angle
 
  args.outputs.background_color = [100, 100, 100]
 
+ size_increase = 4
 
-  dr_logo.stack.times do |index|
+  sprite_stack.stack.times do |index|
     y_offset = (index * 2) * Math.sin(camera_angle * Math::PI / 180)
     args.outputs.sprites << {
-      x: dr_logo.x,
-      y: dr_logo.y + y_offset,
-      w: dr_logo.w * 4,
-      h: dr_logo.h * 4,
-      path: dr_logo.path,
+      x: sprite_stack.x,
+      y: sprite_stack.y + y_offset,
+      w: sprite_stack.w * size_increase,
+      h: sprite_stack.h * size_increase,
+      path: sprite_stack.path,
       source_x: 0,
-      source_y: dr_logo.h * index,
-      source_w: dr_logo.w ,
-      source_h: dr_logo.h ,
+      source_y: sprite_stack.h * index,
+      source_w: sprite_stack.w ,
+      source_h: sprite_stack.h ,
       angle: args.tick_count % 360
     }
   end
